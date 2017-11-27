e900e158efb1fe8672f8d8d43a9e99d5590e720b
Joonas Id <joonas.id@iki.fi>
2017-11-27 16:41:03 +0200

    Make Immutable state unImmutable when rendering, for easier access.

--- src/client/components/StatefulApp.jsx
+++ src/client/components/StatefulApp.jsx
@@ -3,6 +3,20 @@ import { mapValues } from 'lodash'
 
 import App from './App'
 
-const mapStateToProps = (state) => mapValues(state, (value) => value)
+/**
+ * Our state variables are Immutable, with the exception of the redux-little-routers' "router"
+ * property which is plain JS (blasphemy!)
+ */
+const REDUX_ROUTER_KEY = 'router'
+
+const valuesToJS = (value, key) => {
+  return key === REDUX_ROUTER_KEY
+    ? value
+    : value
+      ? value.toJS()
+      : value
+}
+
+const mapStateToProps = (state) => mapValues(state, valuesToJS)
 
 export default connect(mapStateToProps)(App)
